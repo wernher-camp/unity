78f8dfbcee257bfab74ef5b9b16b7871834bdfe8
wernher-camp <158773287+wernher-camp@users.noreply.github.com>
2026-02-04 14:15:33 -0600

    s

--- Assets/scripts/LINKS.cs
+++ Assets/scripts/LINKS.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LINKS : MonoBehaviour
+public class EnlacesDirectos : MonoBehaviour
 {
-    public void links()
-    {
-        
-    Application.OpenURL("https://www.youtube.com/");
+    public Button boton1, boton2, boton3;
 
-    
-}
+    // Se cambió "star" por "Start" (con S mayúscula)
+    public void AbrirPagina(string url)
+    {
+        // Verificamos que los botones no estén vacíos antes de asignarles el link
+        if (boton1 != null) boton1.onClick.AddListener(() => Application.OpenURL("https://www.facebook.com/?locale=es_LA"));
+        if (boton2 != null) boton2.onClick.AddListener(() => Application.OpenURL("https://youtube.com"));
+        if (boton3 != null) boton3.onClick.AddListener(() => Application.OpenURL("https://www.tiktok.com/es-419/"));
+    }
 }
