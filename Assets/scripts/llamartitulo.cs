158c16dbd974ef48585a30f0cc986ff1e4f0c941
wernher-camp <158773287+wernher-camp@users.noreply.github.com>
2026-02-10 15:40:41 -0600

    a

---
+++ Assets/scripts/llamartitulo.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiodeEscena : MonoBehaviour
+{
+    public void CargarEscena(string Escena1)
+    {
+          SceneManager.LoadScene(Escena1);
+    }
+    public void RegresarEscena()
+    {
+        SceneManager.LoadScene("Escena0");
+    }
+}
+
